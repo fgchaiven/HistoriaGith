32be9f929a0d8fbd6a6cd18ab9ce27ba0dc4f348
Usuario <Usuario@192.168.1.205>
2018-02-26 13:07:35 +0100

    uno

--- src/stash.java
+++ src/stash.java
@@ -3,5 +3,6 @@ public class stash {
 int uno;
 int dos;
 int tres;
+int cuatro;
 //equivocacion utiliza la historia para que los cambios se queden al final para que se queden como estaba todo al principio
 }
